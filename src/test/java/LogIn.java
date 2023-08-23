5affc262550a1d7a3d1225de0c4373b9b1a979c3
adieb <mrizqiamiruladieb@gmail.com>
2023-08-23 16:00:25 +0700

    add Log In test

---
+++ src/test/java/LogIn.java
@@ -0,0 +1,68 @@
+import config.env_target;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class LogIn extends env_target {
+    @Test //successful log in
+    public void main(){
+        //Set driver location path
+        System.setProperty("webdriver.chrome.driver","src\\main\\resources\\drivers\\chromedriver.exe");
+        //Maximize driver
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //Set url
+        driver.get(baseUrl);
+        //Set waktu tunggu
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        //Set case stop tunggu
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[contains(text(), 'Customer Login')]"))
+        );
+        //Set element locate
+        //Input Username
+        driver.findElement(By.xpath("//input[@type='text'][@class='input'][@name='username']")).sendKeys("moHawks56");
+        //Input Password
+        driver.findElement(By.xpath("//input[@type='password'][@class='input'][@name='password']")).sendKeys("12345678");
+        //Click Log In Submit Button
+        driver.findElement(By.xpath("//input[@type='submit'][@class='button'][@value='Log In']")).click();
+        //Set case stop tunggu
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[@class='title'][contains(text(), 'Accounts Overview')]"))
+        );
+        //Quit chrome
+        driver.quit();
+    }
+
+    @Test //log in failed
+    public void failed(){
+        //Set driver location path
+        System.setProperty("webdriver.chrome.driver","src\\main\\resources\\drivers\\chromedriver.exe");
+        //Maximize driver
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //Set url
+        driver.get(baseUrl);
+        //Set waktu tunggu
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        //Set case stop tunggu
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[contains(text(), 'Customer Login')]"))
+        );
+        //Set element locate
+        //Click Log In Submit Button
+        driver.findElement(By.xpath("//input[@type='submit'][@class='button'][@value='Log In']")).click();
+        //Set case stop tunggu
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[@class='error'][contains(text(), 'Please enter a username and password.')]"))
+        );
+        //Quit chrome
+        driver.quit();
+    }
+}
